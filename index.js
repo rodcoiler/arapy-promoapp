18f01c371bc42a9c61708127f2b1f5ecb920ebe9
rodcoiler <rodrigozoes@gmail.com>
2026-08-18 12:19:02 -0300

    Add app.js and index.js entrypoints for maximum compatibility with CloudPanel default configurations

---
+++ index.js
@@ -0,0 +1,4 @@
+// index.js
+process.env.PORT = process.env.PORT || 3000;
+process.argv = [process.argv[0], process.argv[1], 'build/server/index.js'];
+import('./node_modules/@remix-run/serve/dist/cli.js');
